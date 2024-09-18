a670266863add40515926c5a034922a611c13b02
mariam2508 <mariamhossam243@gmail.com>
2024-09-18 19:41:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+name=st.text_input('ENTER NAME')
+btn=st.button('show')
+if btn:
+  st.write(f'Hello {name}')

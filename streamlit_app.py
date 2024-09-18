02edbf121c0faf5ddf001ec3f82432022771c10a
mariam2508 <mariamhossam243@gmail.com>
2024-09-18 20:22:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,9 @@
 import streamlit as st
 
+import pandas as pd
+st.header(' file upload app')
+file=st.file_uploader('upload file',type=['csv'])
 
-st.sidebar.title('sidebar')
-area=None
-st.header("calculate area")
-with st.sidebar :
-  choose=st.selectbox('select shape',('circle','rectangle'))
-if choose=='circle':
-  r=st.number_input('enter r',min_value=1,max_value=100)
-  area=3.14*r*r
-  
-elif choose =='rectangle':
-  w=st.number_input('enter w',min_value=1,max_value=100)
-  h=st.number_input('enter h',min_value=1,max_value=100)
-  area=w*h
-  
-bt=st.button('calculate')
-if bt:
-  st.write(f'area={area}')
+if file is not None:
+  df=pd.read_csv(file)
+  st.write(df)

28b47750c43637dc74fcf81f45de46315b85fd37
mariam2508 <mariamhossam243@gmail.com>
2024-09-18 21:57:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,22 +15,19 @@ if file is not None:
     st.write(df[:num_row])
 
 
-num_col = df.select_dtypes(include='number').columns.tolist()
+num_col=df.select_dtypes(include='number').columns.tolist()
+col1,col2,col3=st.columns(3)
+tab1,tab2=st.tabs(['scatter plot','histogram'])
+with tab1:
+  with col1:
+  x_col=st.selectbox('choose x',num_col)
+  with col2:
+  y_col=st.selectbox('choose y',num_col)
+  with col3:
+  color=st.selectbox('choose color',num_col)
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    x_col = st.selectbox('Choose x-axis', num_col)
-with col2:
-    y_col = st.selectbox('Choose y-axis', num_col)
-with col3:
-    color = st.selectbox('Choose color', num_col)
-
-plot_type = st.selectbox('Choose plot type', ['Scatter Plot', 'Histogram'])
-
-if plot_type == 'Scatter Plot':
-    fig = px.scatter(df, x=x_col, y=y_col, color=color)
-    st.plotly_chart(fig)
-elif plot_type == 'Histogram':
-    fig2 = px.histogram(df, x=x_col)
-    st.plotly_chart(fig2)
+  fig=px.scatter(df,x=x_col,y=y_col,color=color)
+  st.plotly_chart(fig)
+with tab2:
+  fig2=px.histogram(df,x=x_col)
+  st.plotly_chart(fig2)

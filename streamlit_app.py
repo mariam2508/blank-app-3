0ace36b3f05f88b2c73c8c8aff6994164187455f
mariam2508 <mariamhossam243@gmail.com>
2024-09-18 20:51:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ file=st.file_uploader('upload file',type=['csv'])
 if file is not None:
   df=pd.read_csv(file)
   st.write(df)
+num_row=st.slider('choose num rows',min_value=1,max_value=len(df))
+st.write(df:head(num_row))

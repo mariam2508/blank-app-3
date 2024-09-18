5578ea06f43383fe5589b9259359a63e801fc640
mariam2508 <mariamhossam243@gmail.com>
2024-09-18 20:13:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
+
+st.sidebar.title('sidebar')
 area=None
 st.header("calculate area")
-choose=st.selectbox('select shape',('circle','rectangle'))
+with st.sidebar :
+  choose=st.selectbox('select shape',('circle','rectangle'))
 if choose=='circle':
   r=st.number_input('enter r',min_value=1,max_value=100)
   area=3.14*r*r
@@ -15,4 +18,3 @@ elif choose =='rectangle':
 bt=st.button('calculate')
 if bt:
   st.write(f'area={area}')
-

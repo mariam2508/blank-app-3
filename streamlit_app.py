b5f2d6e04051eb93c4e8ce3c1f7cf24732dd56e5
mariam2508 <mariamhossam243@gmail.com>
2024-09-18 19:53:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,19 @@ name=st.text_input('ENTER NAME')
 btn=st.button('show')
 if btn:
   st.write(f'Hello {name}')
+area=None
+st.header("calculate area")
+choose=st.selectbox('select shape',('circle','rectangle'))
+if choose=='circle':
+  r=st.number_input('enter r',min_value=1,max_value=100)
+  area=3.14*r*r
+  
+elif choose =='rectangle'
+  w=st.number_input('enter w',min_value=1,max_value=100)
+  h=st.number_input('enter h',min_value=1,max_value=100)
+  area=w*h
+  
+bt=st.button('calculate')
+if bt:
+  st.write(f'area={area}')
+
